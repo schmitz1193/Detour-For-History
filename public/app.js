5c3083643db08742b9a5f1a363d31f01589600be
schmitz1193 <schmitz1193@gmail.com>
2016-07-14 18:17:03 -0500

    closer

--- public/app.js
+++ public/app.js
@@ -2,10 +2,16 @@
 console.log("made to app.js");
 document.getElementById("submitCoords").addEventListener("click", function(){
 
-  console.log("show coords??");
-// var longitude = document.getElementById("long").val;
-// console.log("longitude ", longitude);
+  alert("show coords??");
+var longitude = document.getElementById("long").val;
+console.log("longitude ", longitude);
 
 // var latitude = document.getElementById("lat");
 
 });
+
+
+// document.getElementById("markerText").innerHTML = Text;
+
+
+
