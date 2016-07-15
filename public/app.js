d9af33ce796de761dcb13b1394a57de38251a9bb
schmitz1193 <schmitz1193@gmail.com>
2016-07-15 17:07:28 -0500

    got long from input

--- public/app.js
+++ public/app.js
@@ -3,7 +3,8 @@ console.log("made to app.js");
 document.getElementById("submitCoords").addEventListener("click", function(){
 
   alert("show coords??");
-var longitude = document.getElementById("long").val;
+var longitude = document.getElementById("long").value;
+alert("show long" + longitude);
 console.log("longitude ", longitude);
 
 // var latitude = document.getElementById("lat");
