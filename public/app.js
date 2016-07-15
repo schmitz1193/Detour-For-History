a54db89b5ecd33a0f5a8686b4bae91085d74fc01
schmitz1193 <schmitz1193@gmail.com>
2016-07-15 17:46:51 -0500

    url with values

--- public/app.js
+++ public/app.js
@@ -1,15 +1,19 @@
 
-console.log("made to app.js");
 document.getElementById("submitCoords").addEventListener("click", function(){
-
   alert("show coords??");
 var longitude = document.getElementById("long").value;
 alert("show long" + longitude);
 console.log("longitude ", longitude);
 
-// var latitude = document.getElementById("lat");
+var latitude = document.getElementById("lat").value;
+// values are showing in url but the following two lines are not working!!??
+alert("show lat" + latitude);
+console.log("latitude ", latitude);
+
 
 });
+
+console.log("What is happening?");
 
 
 // document.getElementById("markerText").innerHTML = Text;
