f11b508035e6525bc0103a5a6e5c0ea69635b4b2
schmitz1193 <schmitz1193@gmail.com>
2016-07-17 13:09:25 -0500

    created divs for output

--- public/app.js
+++ public/app.js
@@ -2,8 +2,8 @@
 document.getElementById("submitCoords").addEventListener("click", function(){
   alert("show coords??");
 var longitude = document.getElementById("long").value;
-alert("show long" + longitude);
 console.log("longitude ", longitude);
+alert("show long" + longitude);
 
 var latitude = document.getElementById("lat").value;
 // values are showing in url but the following two lines are not working!!??
@@ -13,7 +13,7 @@ console.log("latitude ", latitude);
 
 });
 
-console.log("What is happening?");
+// console.log("What is happening?");
 
 
 // document.getElementById("markerText").innerHTML = Text;
