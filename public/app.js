67bd36d3c816014b1b37cf153e4c71aef723dc9a
schmitz1193 <schmitz1193@gmail.com>
2016-07-17 14:43:21 -0500

    innerHtml

--- public/app.js
+++ public/app.js
@@ -2,14 +2,15 @@ var longitude = '';
 var latitude = '';
 
 document.getElementById("submitCoords").addEventListener("click", function(){
-longitude = document.getElementById("long").value;
-console.log("longitude ", longitude);
+  longitude = document.getElementById("long").value;
+  console.log("longitude ", longitude);
 
-latitude = document.getElementById("lat").value;
-console.log("latitude ", latitude);
+  latitude = document.getElementById("lat").value;
+  console.log("latitude ", latitude);
   // alert("show coords??");
   console.log("send these coords to API");
   console.log(longitude + " " + latitude);
+  document.getElementById("markerText").innerHTML = "this is the info";
 
 
 });
